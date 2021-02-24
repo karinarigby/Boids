3113e2bca0d410c55b07b71813ca250e5a421bf7
karinarigby <karina.a.rigby@gmail.com>
2021-02-23 18:25:09 -0800

    Init commit

---
+++ Assets/Scripts/BoidR.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Refactored
+{
+    public class BoidR : MonoBehaviour
+    {
+        //the "frontal binocular vision" range of the boid
+        float binocularAreaRange = 30.0f;
+        public float BinocularAreaRangeAngle { get { return binocularAreaRange; } }
+
+        float fullVisionRange = 340.0f;
+        public float FullVisionAngle { get { return fullVisionRange; } }
+
+        public Vector3 Velocity { get; set; }
+        public Vector3 LastPosition { get; set; }
+
+        //This determines the boid's initial speed
+        Vector3 relativeOldPosition = new Vector3(0, 0.1f, 0);
+
+        private void Start()
+        {
+            LastPosition = transform.position - relativeOldPosition;
+            Velocity.Set(0.0f, 0.1f, 0.0f);
+        }
+
+    }
+}
---
+++ Assets/Scripts/BoidSystemR.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Refactored
+{
+    public class BoidSystemR : MonoBehaviour
+    {
+        float ComputeVisualFieldWeight(BoidR thisBoid, BoidR thatBoid)
+        {
+            float binocularAngleOverTwo = thisBoid.BinocularAreaRangeAngle/2;
+            float fullVisionAngleOverTwo = thisBoid.FullVisionAngle/2;
+            float visualAngleMagnitude = Vector3.Angle(thisBoid.Velocity,thatBoid.transform.position);
+
+            if (visualAngleMagnitude > fullVisionAngleOverTwo)
+                return 0;
+
+            if ((binocularAngleOverTwo <= visualAngleMagnitude)&&(visualAngleMagnitude<fullVisionAngleOverTwo))
+                return ((fullVisionAngleOverTwo - visualAngleMagnitude) / (fullVisionAngleOverTwo - binocularAngleOverTwo));
+
+            if ((binocularAngleOverTwo < visualAngleMagnitude) && (visualAngleMagnitude < binocularAngleOverTwo))
+                return 1.0f;
+            return -1;
+
+        }
+    }
+
